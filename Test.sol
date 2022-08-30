6ef26e7022c6561de1aa0c3d1bfeb17252825708
fps <anthony.nnaemeka.umeh@gmail.com>
2022-08-30 07:31:05 +0100

    Update Test.sol

--- Test.sol
+++ Test.sol
@@ -1,23 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
 import "./MathLib.sol";
 
 /*
 * @title: Array Sorter Library.
-* @author: Anthony (fps) https://github.com/fps8k.
-* @dev:
-* 
+* @author: Anthony (fps) https://github.com/0xfps.
+* @dev: Contract to sort arrays in ascending or descending order.
 */
-
-contract f
-{
+contract F {
     using ArraySorter for uint256[];
 
     uint[] public a = [8, 7, 2, 2, 0, 8, 9];
     uint[] public j;
 
-    function m() public
-    {
+    function sortArrInDesc() public {
         j = a.sortDesc();
+    }
+    
+    function sortArrInAsc() public {
+        j = a.sortAsc();
     }
 }
